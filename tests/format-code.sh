c71052bb9cd5939b08479345978a4105787f17d0
Tristan Masselot <tristan.masselot@epitech.eu>
2023-09-26 11:08:01 +0200

    feat: updated clang format script

--- tests/format-code.sh
+++ tests/format-code.sh
@@ -2,6 +2,22 @@
 
 CLANGFORMAT=`which clang-format`
 STYLE="file"
-FILES="source/*.cpp include/*.hpp"
+FILES="source/*.cpp"
 
-$CLANGFORMAT -i --style=${STYLE} ${FILES}
+if [ $# -eq 1 ]; then
+    if [[ $1 == "-h" || $1 == "--help" ]]; then
+        echo "USAGE"
+        echo -e "    ./format-code.sh [-h] [-f]\n"
+        echo "DESCRIPTION"
+        echo -e "    -h, --help\tShows this message."
+        echo -e "    -f, --fix\tMake formatting changes."
+        exit 0
+    fi
+    if [[ $1 == "-f" || $1 == "--fix" ]]; then
+        $CLANGFORMAT -i --style=${STYLE} ${FILES}
+        exit 0
+    fi
+    exit 1
+fi
+
+$CLANGFORMAT -i -n --Werror --style=${STYLE} ${FILES}
