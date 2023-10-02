c93f2c837029b85c11fc70fbae36d5338b6c887b
Nestyles <tom.sancho@epitech.eu>
2023-10-02 11:35:11 +0200

    [ADD] Conan package manager with raylib ;)

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-    std::cout << "Hello world :,D!" << std::endl;
+    InitWindow(800, 450, "raylib [core] example - basic window");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText(
+            "Congrats! You created your first window!", 190, 200, 20,
+            LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
