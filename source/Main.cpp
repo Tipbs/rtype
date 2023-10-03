6c9360b5aabbfba9cae1124fe7fc97dfa4247d9f
Nestyles <tom.sancho@epitech.eu>
2023-10-03 14:53:04 +0200

    [REWORK] Temporary remove raylib to make everything work with github actions

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include <raylib.h>
+//#include <raylib.h>
 
 int main()
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText(
-            "Congrats! You created your first window!", 190, 200, 20,
-            LIGHTGRAY);
-        EndDrawing();
-    }
-    CloseWindow();
+    //InitWindow(800, 450, "raylib [core] example - basic window");
+    //while (!WindowShouldClose()) {
+    //    BeginDrawing();
+    //    ClearBackground(RAYWHITE);
+    //    DrawText(
+    //        "Congrats! You created your first window!", 190, 200, 20,
+    //        LIGHTGRAY);
+    //    EndDrawing();
+    //}
+    //CloseWindow();
     return 0;
 }
