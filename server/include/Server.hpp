1d4d3ba1b9d524d712460027db51545a8720f4c9
TAILLAN Lucas <lucas.taillan@epitech.eu>
2023-10-03 17:20:11 +0200

    [ADD] broadcast to all connected clients

--- server/include/Server.hpp
+++ server/include/Server.hpp
@@ -1,17 +1,12 @@
+#include <map>
+#include <thread>
+#include <boost/asio/deadline_timer.hpp>
+#include <boost/asio/io_context.hpp>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/ip/udp.hpp>
 #include <boost/array.hpp>
-
-struct pos {
-    int x;
-    int y;
-};
-
-struct test_struct {
-    int type;
-    int id;
-    struct pos pos;
-};
+#include <chrono>
+#include "../../shared/UserCmd.hpp"
 
 struct Message {
     int type;
@@ -19,14 +14,24 @@ struct Message {
 
 class udp_server {
     public:
-        udp_server(boost::asio::io_context &_svc, std::size_t port);
+        udp_server(std::size_t port);
         ~udp_server();
         void handle_receive(const boost::system::error_code &, std::size_t);
         void handle_response();
         void handle_send(const boost::system::error_code &, std::size_t);
+        void broadcast();
+        void handle_broadcast(const boost::system::error_code &, std::size_t);
+        void multiple_broadcast(std::vector<boost::asio::ip::udp::endpoint>, std::map<std::size_t ,std::vector<UserCmd>>);
+        void add_buff_to_cmd();
     private:
         void start_receive();
+
+        std::size_t _port;
+        std::size_t id = 0;
+        boost::asio::io_context _svc;
         boost::asio::ip::udp::socket _socket;
         boost::asio::ip::udp::endpoint _remote_point;
+        std::vector<boost::asio::ip::udp::endpoint> clients;
+        std::map<std::size_t, std::vector<UserCmd>> cmd;
         boost::array<char, 512> _recv_buffer;
 };
--- server/source/Server.cpp
+++ server/source/Server.cpp
@@ -1,36 +1,82 @@
 #include <boost/asio.hpp>
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/deadline_timer.hpp>
 #include <boost/asio/io_context.hpp>
+#include <boost/asio/read.hpp>
+#include <boost/asio/streambuf.hpp>
 #include <boost/bind.hpp>
+#include <cstddef>
 #include <cstring>
+#include <deque>
 #include <exception>
 #include <iostream>
+#include <map>
+#include <string>
+#include <thread>
 #include "../include/Server.hpp"
 
 using boost::asio::ip::udp;
 
-void udp_server::handle_send(const boost::system::error_code &, std::size_t bytes_transferred)
-{
-    std::cout << "bytes_transferred = " << bytes_transferred << std::endl;
-    start_receive();
-}
+typedef std::deque<boost::asio::const_buffer> ConstBuffers;
 
-void udp_server::handle_receive(const boost::system::error_code &error, std::size_t bytes_transferred)
+void udp_server::handle_send(const boost::system::error_code &error, std::size_t bytes_transferred) //Callback to the send function 
 {
-    if (!error || error == boost::asio::error::message_size) {
-        Message type;
-        std::memcpy(&type, &_recv_buffer, sizeof(Message));
-        test_struct test;
-        test.type = 2;
-        test.id = 7;
-        test.pos = {1, 13};
-        _socket.async_send_to(boost::asio::buffer(&test, sizeof(test)), _remote_point,
-            boost::bind(&udp_server::handle_send, this,
-                boost::asio::placeholders::error,
-                boost::asio::placeholders::bytes_transferred));
+    if (!error) {
+        std::cout << "bytes_transferred = " << bytes_transferred << std::endl;
+        start_receive();
     }
 }
 
-void udp_server::start_receive()
+void udp_server::handle_broadcast(const boost::system::error_code &error, std::size_t bytes_transferred) //Callback to broadcast
+{
+    if (!error) {
+        std::cout << "bytes_transferred = " << bytes_transferred << std::endl;
+    }
+}
+
+void udp_server::multiple_broadcast(std::vector<udp::endpoint> tmp, std::map<std::size_t, std::vector<UserCmd>> commands)
+{
+    Message test;
+    test.type = 1;
+    for (const auto& client_endpoint : tmp) {
+        _socket.async_send_to(boost::asio::buffer(&test, sizeof(test)), client_endpoint,
+            boost::bind(&udp_server::handle_broadcast, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+}
+
+void udp_server::broadcast() // Broadcast a message to all connected clients that already sent a message 
+{
+    std::vector<udp::endpoint> tmp = clients;
+    std::thread broadcasting(&udp_server::multiple_broadcast, this, tmp, std::move(cmd));
+    broadcasting.detach();
+}
+
+void udp_server::add_buff_to_cmd()
+{
+    std::size_t i = 0;
+    for (const auto &client_endpoint : clients)
+        i++;
+    UserCmd tmp;
+    std::memcpy(&tmp, &_recv_buffer, sizeof(UserCmd));
+    cmd[i - 1].push_back(tmp);
+}
+
+void udp_server::handle_receive(const boost::system::error_code &error, std::size_t bytes_transferred) // Callback to the receive function
+{
+    if (!error || error == boost::asio::error::message_size) {
+        std::cout << "Received " << bytes_transferred << "bytes" << std::endl;
+        if (std::find(clients.begin(), clients.end(), _remote_point) == clients.end() && clients.size() <= 4) {
+            clients.push_back(_remote_point);
+        }
+        add_buff_to_cmd();
+        broadcast();
+        start_receive();
+    }
+}
+
+void udp_server::start_receive() // Receive function
 {
     _socket.async_receive_from(
         boost::asio::buffer(_recv_buffer), _remote_point,
@@ -39,9 +85,12 @@ void udp_server::start_receive()
         boost::asio::placeholders::bytes_transferred));
 }
 
-udp_server::udp_server(boost::asio::io_service & svc, std::size_t port) : _socket(svc, udp::endpoint(udp::v4(), port))
+udp_server::udp_server(std::size_t port) : _svc(), _socket(_svc, udp::endpoint(udp::v4(), port))
 {
+    _port = port;
+    _socket.non_blocking(true);
     start_receive(); 
+    _svc.run();
 }
 
 udp_server::~udp_server()
@@ -49,12 +98,13 @@ udp_server::~udp_server()
     _socket.close();
 }
 
-int main()
+int main(int ac, char **av)
 {
     try {
-        boost::asio::io_context svc;
-        udp_server server(svc, 5000);
-        svc.run();
+        std::size_t port = 5000;
+        if (ac == 2 && std::stoi(av[1]))
+            port = std::stoi(av[1]);
+        udp_server server(port);
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
     }
