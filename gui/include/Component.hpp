521fdcaec69de71f3dcf4e2e5636da18e9235412
Aurelien Masset <aurelien.masset@epitech.eu>
2023-10-04 15:51:36 +0200

    Added components

---
+++ gui/include/Component.hpp
@@ -0,0 +1,54 @@
+#include "raylib.h"
+
+#ifndef COMPONENT_HPP
+    #define COMPONENT_HPP
+
+    struct Position {
+        int pos_X = 0;
+        int pos_Y = 0;
+    };
+    struct Damages {
+        int damages = 0;
+    };
+    struct Size {
+        int size_X = 1;
+        int size_Y = 1;
+    };
+    struct Health {
+        int health = 1;
+    };
+    struct Speed {
+        float speed = 0;
+    };
+    struct Direction {
+        float dir_X = 0;
+        float dir_Y = 0;
+    };
+    struct SpawnGrace {
+        float timer = 0;
+        float creation_time = 0;
+    };
+    struct Sprite {
+        Texutre2D sprite;
+    };
+    struct Animation {
+        Rectangle rect; //contains size as well
+        Sprite sprite;
+        Position* frames; // contains a list of position for each frames of an animated picture.
+    };
+    struct Ammo {
+        Animation sprite;
+        Speed speed;
+        Damages damages;
+        Size size;
+        Health health;
+        Direction dir;
+    };
+    struct Weapon {
+        int type;
+        Size size;
+        float attack_speed;
+        Animation sprite;
+        Ammo ammo;
+    };
+#endif
