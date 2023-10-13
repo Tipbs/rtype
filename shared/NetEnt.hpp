0c0d75e09d9011fef5ff3f5a140fc2ea4035b147
Nestyles <tom.sancho@epitech.eu>
2023-10-13 14:34:48 +0200

    [ADD] EntityType enum to NetEnt (will require refactoring everywhere)

--- shared/NetEnt.hpp
+++ shared/NetEnt.hpp
@@ -4,15 +4,21 @@
 #include <mutex>
 #include "Utils.hpp"
 
+enum class EntityType {
+    Player
+};
+
 class NetEnt {
 public:
     int id;
+	EntityType type;
     Utils::Vec2 pos;
     uint8_t states;
 
     template<class Archive>
     void serialize(Archive &ar, const unsigned int version) {
         ar &id;
+        ar &type;
         ar &pos;
         ar &states;
     };
