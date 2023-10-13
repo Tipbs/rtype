60e6265eb8e9b364c4b5125e6b77848cdebcd8ad
Nestyles <tom.sancho@epitech.eu>
2023-10-13 14:26:41 +0200

    [ADD] Thread safe version of NetEnt for client

--- shared/NetEnt.hpp
+++ shared/NetEnt.hpp
@@ -1,9 +1,24 @@
 #pragma once
 #include <cstdint>
+#include <boost/serialization/vector.hpp>
+#include <mutex>
 #include "Utils.hpp"
 
 class NetEnt {
+public:
     int id;
     Utils::Vec2 pos;
     uint8_t states;
+
+    template<class Archive>
+    void serialize(Archive &ar, const unsigned int version) {
+        ar &id;
+        ar &pos;
+        ar &states;
+    };
+};
+
+struct ThreadNetEnt {
+    std::vector<NetEnt> ents;
+    std::mutex mutex;
 };
