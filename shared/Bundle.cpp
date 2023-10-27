691c3250e0c1a104494df12baba36e0e83251652
Nestyles <tom.sancho@epitech.eu>
2023-10-27 15:59:12 +0200

    [ADD] New constructor for Ammo with custom direction

--- shared/Bundle.cpp
+++ shared/Bundle.cpp
@@ -32,7 +32,9 @@ size_t create_player(Registry &reg, size_t id, Position pos)
     return (size_t) new_entity;
 }
 
-void create_ammo(Registry &reg, Position pos, float damage_mult, int color_id)
+void create_ammo(
+    Registry &reg, Position pos, Direction diro,
+    float damage_mult, int color_id)
 {
     Entity const new_entity = reg.spawn_entity();
     int hitwidth = 120 * (damage_mult / 2);
@@ -43,7 +45,39 @@ void create_ammo(Registry &reg, Position pos, float damage_mult, int color_id)
     Speed speedo(450);
     Damages damag(damage_mult);
     Health healt(1);
-    Direction diro = Direction(1, 0); // std::pow(-1, rand()) * rand()
+    Position posCopy(
+        pos.pos_X - (float) hitwidth / 2, pos.pos_Y - (float) hitheight / 2);
+    #ifndef SERVER
+    Sprite sprite(
+        path.c_str(), hitwidth, hitheight, 8, 5); // width height are the visual box
+
+    sprite.color_id = color_id;
+    #endif
+
+    reg.add_component(
+        new_entity, std::move(posCopy));
+    reg.add_component(new_entity, std::move(Size));
+    #ifndef SERVER
+    reg.add_component(new_entity, std::move(sprite));
+    #endif
+    reg.add_component(new_entity, std::move(speedo));
+    reg.add_component(new_entity, std::move(diro));
+    reg.add_component(new_entity, std::move(damag));
+    reg.add_component(new_entity, std::move(healt));
+}
+
+void create_ammo(Registry& reg, Position pos, float damage_mult, int color_id)
+{
+    Entity const new_entity = reg.spawn_entity();
+    int hitwidth = 120 * (damage_mult / 2);
+    int hitheight = 24 * (damage_mult / 2);
+    Size Size(hitwidth, hitheight); // hitbox
+    std::string path =
+        "./gui/ressources/Sprites/shoot_ammo.png"; // fichier de la munition
+    Speed speedo(450);
+    Damages damag(damage_mult);
+    Direction diro(1, 0);
+    Health healt(1);
     Position posCopy(
         pos.pos_X - (float) hitwidth / 2, pos.pos_Y - (float) hitheight / 2);
     #ifndef SERVER
