5796404bf864c68f3c701d8cb320a2af69e52a1a
Tipbs <thibaut.degodenne@epitech.eu>
2023-11-04 22:48:27 +0100

    [FIX] Deleted main

--- gui/source/Main.cpp
+++ gui/source/Main.cpp
@@ -13,6 +13,44 @@
 
 int main(int ac, char **av)
 {
-    Colision col(Tag::Player, Tag::Enemy);
-    std::cout << col.to_string() << std::endl;
+    const int ScreenWidth = 1280;
+    const int ScreenHeight = 720;
+    // const int ScreenWidth = 900;
+    // const int ScreenHeight = 1000;
+    boost::asio::io_context context;
+    Registry reg;
+    std::string port = "5000";
+    std::string ip = "127.0.0.1";
+    if (ac == 3 && std::stoi(av[2])) {
+        ip = av[1];
+        port = av[2];
+    }
+    udp_client net_client(context, ip, port, reg);
+    context.run();
+    InitWindow(ScreenWidth, ScreenHeight, "R-Type");
+    InitAudioDevice();
+    SetTargetFPS(60);
+
+    Factory factory(reg);
+
+    factory.register_components();
+    factory.create_background(ScreenWidth, ScreenHeight);
+    auto net_player_info = net_client.get_player_id();
+    factory.create_points(Position(200, 200), 10, 10);
+    Entity player =
+        factory.create_player(net_player_info.pos, net_player_info.id);
+    reg.emplace_component<Current_Player>(player);
+    std::cout << "player pos id: " << net_player_info.id << std::endl;
+    std::cout << "player pos x: " << net_player_info.pos.x << std::endl;
+    std::cout << "player pos y: " << net_player_info.pos.y << std::endl;
+    Entity weapon = factory.create_weapon(player);
+    factory.create_hud(ScreenWidth, ScreenHeight, player, weapon);
+    factory.create_sounds(reg);
+    factory.add_systems();
+
+    while (!WindowShouldClose()) {
+        reg.run_systems();
+        context.poll();
+        context.reset();
+    }
 }
